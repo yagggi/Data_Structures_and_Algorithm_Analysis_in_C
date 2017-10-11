e5d02c6b1d95d51e7790639f6de4e4cbd8f59123
fakepoet <fakepoet0101@gmail.com>
2017-10-12 01:49:46 +0800

    add gcd

---
+++ Data_Structures_and_Algorithm_Analysis_in_C/chapter 2/max_common_divisor.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+def gcd(m, n):
+    """
+    :type m: int.
+
+    :type n: int
+    :rtype: int
+    """
+    while n > 0:
+        rem = m % n
+        m = n
+        n = rem
+    return m
+
+
+if __name__ == "__main__":
+    from utils import profile
+    import random
+
+    test_arg1, test_arg2 = random.randint(10000, 1000000), random.randint(1, 10000)
+    print profile(gcd, test_arg1, test_arg2)
