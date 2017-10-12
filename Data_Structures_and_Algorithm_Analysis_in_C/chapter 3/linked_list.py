7491534cb4fc1eb1bebc4501e213fbeb3312e4db
fakepoet <fakepoet0101@gmail.com>
2017-10-13 01:59:11 +0800

    add linked_list

---
+++ Data_Structures_and_Algorithm_Analysis_in_C/chapter 3/linked_list.py
@@ -0,0 +1,83 @@
+# coding: utf-8
+
+
+class SingleLinkedList(object):
+
+    def __init__(self, next_node=None, value=None):
+        self.next = next_node
+        self.value = value
+
+    def is_last(self):
+        return self.next is None
+
+    @staticmethod
+    def find(ele, head):
+        position = head.next
+        while position is not None and position.value != ele:
+            position = position.next
+        return position
+
+    @staticmethod
+    def find_prev(ele, head):
+        position = head
+        while position.next is not None and position.next.value != ele:
+            position = position.next
+        return position
+
+    @staticmethod
+    def delete(ele, head):
+        import gc
+
+        prev_node = SingleLinkedList.find_prev(ele, head)
+        temp_node = SingleLinkedList()
+        temp_node = prev_node.next
+        prev_node.next = temp_node.next
+        del temp_node
+        gc.collect()
+        return
+
+    @staticmethod
+    def insert(ele, head, position):
+        temp_node = SingleLinkedList()
+        temp_node.next = position.next
+        temp_node.value = ele
+        position.next = temp_node
+        return
+
+
+class DoublyLinkedList(object):
+
+    def __init__(self, next_node=None, prev_node=None, value=None):
+        self.next = next_node
+        self.prev = prev_node
+        self.value = value
+
+    def is_last(self):
+        return self.next is None
+
+    @staticmethod
+    def find(ele, head):
+        position = head.next
+        while position is not None and position.value != ele:
+            position = position.next
+        return position
+
+    @staticmethod
+    def delete(ele, head):
+        import gc
+
+        node = DoublyLinkedList.find(ele, head)
+        prev_node = node.prev
+        prev_node.next = node.next
+        del node
+        gc.collect()
+        return
+
+    @staticmethod
+    def insert(ele, head, position):
+        temp_node = DoublyLinkedList()
+        temp_node.next = position
+        temp_node.prev = position.prev
+        temp_node.value = ele
+        position.prev.next = temp_node
+        return
