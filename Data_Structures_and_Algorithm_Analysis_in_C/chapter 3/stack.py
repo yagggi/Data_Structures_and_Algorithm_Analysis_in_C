bad8eba41448e7fafe2bde05fe3e0a4b7c07f9cf
fakepoet <fakepoet0101@gmail.com>
2017-10-17 02:31:12 +0800

    add ArrayStack

--- Data_Structures_and_Algorithm_Analysis_in_C/chapter 3/stack.py
+++ Data_Structures_and_Algorithm_Analysis_in_C/chapter 3/stack.py
@@ -21,7 +21,26 @@ class ListStack(SinglyLinkedList):
         return stack.next.value
 
 
+class ArrayStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.top_index = -1
+
+    def push(self, ele):
+        self.stack.append(ele)
+        self.top_index += 1
+
+    def pop(self):
+        self.top_index -= 1
+        return self.stack.pop()
+
+    def top(self):
+        return self.stack[self.top_index]
+
+
 if __name__ == "__main__":
+    print '---ListStack---'
     s = ListStack()
     ListStack.push(1, s)
     ListStack.push(2, s)
@@ -29,3 +48,11 @@ if __name__ == "__main__":
     ListStack.pop(s)
     SinglyLinkedList.print_list(s)
     print ListStack.top(s)
+    print '---ArrayStack---'
+    s2 = ArrayStack()
+    s2.push(1)
+    s2.push(2)
+    print s2.stack
+    s2.pop()
+    print s2.stack
+    print s2.top()
