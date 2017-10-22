720047a4480b0e17b62ebb2da6927c9f3e2ee89b
fakepoet <fakepoet0101@gmail.com>
2017-10-23 01:34:06 +0800

    add queue

---
+++ Data_Structures_and_Algorithm_Analysis_in_C/chapter 3/queue.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+
+class Queue(object):
+
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.size = 0
+        self.queue = []
+
+    def is_full(self):
+        return self.size == self.max_size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, ele):
+        assert not self.is_full(), 'queue is full.'
+        self.size += 1
+        self.queue.insert(0, ele)
+
+    def dequeue(self):
+        assert not self.is_empty(), 'queue is empty.'
+        return self.queue.pop()
+
+
+if __name__ == "__main__":
+    q = Queue(3)
+    try:
+        q.dequeue()
+    except AssertionError as e:
+        print e.message
+
+    q.enqueue(1)
+    q.enqueue(2)
+    q.enqueue(3)
+    print q.queue
+    try:
+        q.enqueue(4)
+    except AssertionError as e:
+        print e.message
