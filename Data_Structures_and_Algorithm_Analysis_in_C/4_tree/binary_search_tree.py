8e35e8007b62f066909ac84ba8fb770fee1df0bc
fakepoet <fakepoet0101@gmail.com>
2018-04-25 22:51:16 +0800

    rename

---
+++ Data_Structures_and_Algorithm_Analysis_in_C/4_tree/binary_search_tree.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+
+class BinaryTree(object):
+
+    def __init__(self, value=None, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def 
