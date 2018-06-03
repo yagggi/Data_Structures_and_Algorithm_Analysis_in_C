502320f302e53fae05fcb311750015df70e279f2
fakepoet <fakepoet0101@gmail.com>
2018-06-03 16:43:24 +0800

    [FIX] fix bugs; add depth method; working on verbose method.

--- Data_Structures_and_Algorithm_Analysis_in_C/4_tree/binary_search_tree.py
+++ Data_Structures_and_Algorithm_Analysis_in_C/4_tree/binary_search_tree.py
@@ -13,7 +13,11 @@ class BinarySearchTree:
         if self.value == x:
             return self
         if self.value > x:
+            if not self.left:
+                return None
             return self.left.find(x)
+        if not self.right:
+            return None
         return self.right.find(x)
 
     def find_max(self):
@@ -31,5 +35,31 @@ class BinarySearchTree:
             self.value = x
             return self
         if self.value > x:
+            if not self.left:
+                self.left = BinarySearchTree()
             return self.left.insert(x)
+
+        if not self.right:
+            self.right = BinarySearchTree()
         return self.right.insert(x)
+
+    def depth(self, depth=0):
+        if self.left and not self.right:
+            return self.left.depth(depth + 1)
+        elif self.left and self.right:
+            return max(self.left.depth(depth + 1), self.right.depth(depth + 1))
+        elif self.right and not self.left:
+            return self.right.depth(depth + 1)
+        else:
+            return depth
+
+    def verbose(self, depth=0, width=0, nodes=None):
+        if not width:
+            max_depth = self.depth()
+            verbose_depth = 2 * max_depth + 1
+            width = 2 ** max_depth
+        if not nodes:
+            nodes = [self,]
+
+        if depth == max_depth:
+            return
