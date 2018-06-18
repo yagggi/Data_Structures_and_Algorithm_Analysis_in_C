0ad7c8608ef6a9a5ec16a60b7876785d867c3709
fakepoet <fakepoet0101@gmail.com>
2018-06-19 02:45:31 +0800

    [ADD] add AVLTree's insert method(including single and double rotate).

--- Data_Structures_and_Algorithm_Analysis_in_C/4_tree/avl_tree.py
+++ Data_Structures_and_Algorithm_Analysis_in_C/4_tree/avl_tree.py
@@ -8,5 +8,66 @@ class AVLTree:
         self.left = left
         self.hight = hight
 
-    def check_balance(self):
+    @staticmethod
+    def hight(node):
+        if node is None:
+            return -1
+        else:
+            return node.hight
+
+    @staticmethod
+    def up(node, hight):
+        if node is None:
+            return
+        node.hight += 1
+        AVLTree.up(node.left, hight)
+        AVLTree.up(node.right, hight)
         return
+
+    @staticmethod
+    def left_single_rotate(node):
+        left = node.left
+        left.right = node
+        left.hight -= 1
+        AVLTree.up(left.left, -1)
+        AVLTree.up(left.right, 1)
+        return left
+
+    @staticmethod
+    def right_single_rotate(node):
+        right = node.right
+        right.left = node
+        right.hight -= 1
+        AVLTree.up(right.right, -1)
+        AVLTree.up(right.left, 1)
+        return right
+
+    @staticmethod
+    def left_double_rotate(node):
+        node.left = AVLTree.right_single_rotate(node.left)
+        return AVLTree.left_single_rotate(node)
+
+    @staticmethod
+    def right_double_rotate(node):
+        node.right = AVLTree.left_single_rotate(node.right)
+        return AVLTree.right_single_rotate(node)
+
+    @staticmethod
+    def insert(node, x):
+        if node is None:
+            node = AVLTree(hight=0, value=x)
+        elif node.value > x:
+            node.left = AVLTree.insert(node.left, x)
+            if AVLTree.hight(node.left) - AVLTree.hight(node.right) == 2:
+                if x < node.left.value:
+                    node = AVLTree.left_single_rotate(node)
+                else:
+                    node = AVLTree.left_double_rotate(node)
+        elif node.value < x:
+            node.right = AVLTree.insert(node.right, x)
+            if AVLTree.hight(node.right) - AVLTree.hight(node.left) == 2:
+                if x > node.right.value:
+                    node = AVLTree.right_single_rotate(node)
+                else:
+                    node = AVLTree.right_double_rotate(node)
+        return node
