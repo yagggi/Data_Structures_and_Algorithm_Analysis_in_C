203a3c41fa21d93e468a689fd003dcb4835cdbd8
wangyu <wangyu@luojilab.com>
2018-10-24 18:03:40 +0800

    [IMP] improve heap implement; add heap sort.

--- Data_Structures_and_Algorithm_Analysis_in_C/c7_sorting/heap_sort.py
+++ Data_Structures_and_Algorithm_Analysis_in_C/c7_sorting/heap_sort.py
@@ -6,17 +6,21 @@ class MaxHeapForSort:
         if not elements:
             raise Exception('Elements must not be empty!')
         self.cap = len(elements)
-        self.heap = {k: None for k in range(1, self.cap + 1)}
+        self.heap = [None] * (self.cap + 1)
         self.size = 0
         self.elements = elements
 
     def insert(self, val):
-        if self.sieze >= self.cap:
+        if self.size >= self.cap:
             raise Exception('This heap is FULL!')
+        if self.size == 0:
+            self.heap[1] = val
+            self.size += 1
+            return
         ind = self.size + 1
-        while self.heap[ind // 2] < val and ind > 1:
-            self.heap[ind], self.heap[ind // 2] = self.heap[ind // 2], self.heap[ind]
-            ind //= 2
+        while self.heap[ind // 2] is not None and self.heap[ind // 2] < val:
+            self.heap[ind] = self.heap[ind // 2]
+            ind = ind // 2
         self.heap[ind] = val
         self.size += 1
 
@@ -24,19 +28,20 @@ class MaxHeapForSort:
         if self.size <= 0:
             raise Exception('This heap is EMPTY!')
         max_ele = self.heap[1]
-        last_ele = self.heap.pop(self.size)
+        self.heap[1] = None
+        last_ele = self.heap[self.size]
         self.heap[self.size] = max_ele
         self.size -= 1
         ind = 1
         while ind * 2 < self.size:
             left_ind = ind * 2
-            if left_ind + 1 < self.size and self.heap[left_ind + 1] > self.heap[left_ind]:
-                next_ind = left_ind + 1
-            else:
-                next_ind = left_ind
-            if last_ele < self.heap[next_ind]:
-                self.heap[left_ind], self.heap[ind] = self.heap[ind], self.heap[left_ind]
-                ind = next_ind
+            if left_ind != self.size and\
+                    self.heap[left_ind + 1] is not None and\
+                    self.heap[left_ind + 1] > self.heap[left_ind]:
+                left_ind += 1
+            if last_ele < self.heap[left_ind]:
+                self.heap[ind] = self.heap[left_ind]
+                ind = left_ind
             else:
                 break
         self.heap[ind] = last_ele
@@ -45,9 +50,11 @@ class MaxHeapForSort:
     def sort(self):
         for x in self.elements:
             self.insert(x)
-        while True:
-            try:
-                self.delete_max()
-            except Exception:
-                break
-        return list(self.heap.values())
+        while self.size > 0:
+            self.delete_max()
+        return list(filter(lambda ele: ele is not None, self.heap))
+
+
+if __name__ == "__main__":
+    s = MaxHeapForSort([int(x) for x in input().split()])
+    print(s.sort())
