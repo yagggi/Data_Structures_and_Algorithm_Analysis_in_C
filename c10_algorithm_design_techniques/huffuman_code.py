de9fcf3d839e4789721c54e003939465e071d9dc
fakepoet <fakepoet0101@gmail.com>
2018-11-07 15:32:48 +0800

    [ADD] add huffman code; fix heap's delete_min method.

---
+++ c10_algorithm_design_techniques/huffuman_code.py
@@ -0,0 +1,56 @@
+import sys
+sys.path.append("..")
+from c6_priority_queue.binary_heap import MinHeap
+
+
+class HuffmanTree:
+    def __init__(self, val, frequency, left=None, right=None):
+        self.val = val
+        self.freq = frequency
+        self.left = left
+        self.right = right
+
+
+class Huffman:
+    def __init__(self, chrs):
+        self.chrs = chrs
+        self.freq_dic = {}
+        for chr in self.chrs:
+            if not self.freq_dic.get(chr):
+                self.freq_dic[chr] = 1
+            else:
+                self.freq_dic[chr] += 1
+        self.nodes = MinHeap(len(self.freq_dic))
+        for chr, freq in self.freq_dic.items():
+            self.nodes.insert(freq, HuffmanTree(chr, freq))
+        self.root = self.compression()
+        self.code_dic = {}
+        self.__init_code_dic(self.root)
+
+    def min_2(self):
+        e1 = self.nodes.delete_min()['ele']
+        e2 = self.nodes.delete_min()['ele']
+        return e1, e2
+
+    def compression(self):
+        while self.nodes.size > 1:
+            e1, e2 = self.min_2()
+            new_node = HuffmanTree(None, e1.freq + e2.freq, e1, e2)
+            self.nodes.insert(new_node.freq, new_node)
+        return self.nodes.delete_min()['ele']
+
+    def __init_code_dic(self, node=None, pre=''):
+        if not node:
+            return
+        if node.val:
+            self.code_dic[node.val] = pre
+        if node.left:
+            self.__init_code_dic(node.left, pre + '0')
+        if node.right:
+            self.__init_code_dic(node.right, pre + '1')
+
+
+if __name__ == '__main__':
+    s = 'sdfasdfsdfffffdsdsdf'
+    h = Huffman(s)
+    print(h.code_dic, h.freq_dic)
--- c6_priority_queue/binary_heap.py
+++ c6_priority_queue/binary_heap.py
@@ -7,39 +7,53 @@ class MinHeap:
         self.cap = cap
         self.size = 0
 
-    def insert(self, val):
+    def insert(self, val, ele=None):
         if self.size >= self.cap:
             raise Exception('This heap is FULL!')
         if self.size == 0:
-            self.heap[1] = val
+            self.heap[1] = dict(val=val, ele=ele)
             self.size += 1
             return
         ind = self.size + 1
-        while self.heap[ind // 2] is not None and self.heap[ind // 2] > val:
+        while self.heap[ind // 2] is not None and self.heap[ind // 2]['val'] > val:
             self.heap[ind] = self.heap[ind // 2]
             ind = ind // 2
-        self.heap[ind] = val
+        self.heap[ind] = dict(val=val, ele=ele)
         self.size += 1
 
     def delete_min(self):
         if self.size <= 0:
             raise Exception('This heap is EMPTY!')
-        min_ele = self.heap[1].val
+        min_ele = self.heap[1]
         self.heap[1] = None
         last_ele = self.heap[self.size]
         self.heap[self.size] = None
         self.size -= 1
         ind = 1
-        while ind * 2 < self.size:
+        while ind * 2 <= self.size:
             left_ind = ind * 2
             if left_ind != self.size and\
                     self.heap[left_ind + 1] is not None and\
-                    self.heap[left_ind + 1] < self.heap[left_ind]:
+                    self.heap[left_ind + 1]['val'] < self.heap[left_ind]['val']:
                 left_ind += 1
-            if last_ele > self.heap[left_ind]:
+            if last_ele['val'] > self.heap[left_ind]['val']:
                 self.heap[ind] = self.heap[left_ind]
                 ind = left_ind
             else:
                 break
         self.heap[ind] = last_ele
         return min_ele
+
+
+if __name__ == "__main__":
+    h = MinHeap()
+    h.insert(5)
+    h.insert(6)
+    h.insert(8)
+    h.insert(1)
+    h.delete_min()
+    h.delete_min()
+    h.insert(6)
+    print(h.heap)
+    while h.size:
+        print(h.delete_min(), h.size)
